b11eaea5d02561ef9923037e45925dea481fdccc
Calvin Cheng <linchuan.cheng@gmail.com>
2015-02-15 17:54:42 +0800

    Basic Server

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+'use strict';
+
 var http = require('http');
 
-var server = http.createServer();
+var server = http.createServer(function(request, response){
+  console.log('Connection');
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('hello world');
+  response.end();
+});
 server.listen(8001);
