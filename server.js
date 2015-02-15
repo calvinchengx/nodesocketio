534f131e273b60d38b41a648df06c63c5555643a
Calvin Cheng <linchuan.cheng@gmail.com>
2015-02-15 19:55:25 +0800

    Basic http server

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 'use strict';
 
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 var server = http.createServer(function(request, response){
   console.log('Connection');
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('hello world');
-  response.end();
+  var path = url.parse(request.url).pathname;
+  console.log(path);
+
+  switch(path) {
+    case '/':
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write('hello world');
+      response.end();
+      break;
+    case '/socket.html':
+      fs.readFile(__dirname + path, function(error, data) {
+        if (error) {
+          response.writeHead(404);
+          response.write('opps this doesn\'t exist - 404');
+        } else {
+          response.writeHead(200, {'Content-Type': 'text/html'});
+          response.write(data, 'utf8');
+        }
+        response.end();
+      });
+      break;
+    default: 
+      response.writeHead(404);
+      response.write('opps this does\'t exist - 404');
+      response.end();
+      break;
+  }
+
 });
+
 server.listen(8001);
